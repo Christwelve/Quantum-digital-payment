bb0f09c8bd659fe062c3d3d890f9c333bad92393
shadwad <150861786+shadwad@users.noreply.github.com>
2024-02-15 15:40:39 +0100

    client/merchant side added to Bank.py

--- Bank.py
+++ Bank.py
@@ -7,40 +7,54 @@ from netqasm.sdk.epr_socket import EPRSocket
 
 from squidasm.sim.stack.program import Program, ProgramContext, ProgramMeta
 
-
-class ClientPrograme(Program):
-    def __init__(self, name: str, server_name: str):
-        self.name = name
-        self.server_name = server_name
+class BankPrograme(Program):
+    def __init__(self, client_name: str, merchant_name: str):
+        self.client = client_name
+        self.merchant = merchant_name
 
     @property
     def meta(self) -> ProgramMeta:
         return ProgramMeta(
-            name="tutorial_program",
-            csockets=[self.server_name],
-            epr_sockets=[self.server_name],
+            name="sqdp_program",
+            csockets=self.client,
+            epr_sockets=self.client,
+            msockets=self.merchant,
             max_qubits=1,
         )
 
     def run(self, context: ProgramContext):
+        connection: BaseNetQASMConnection = context.connection
+
+        #client side
+        
         # get classical socket to peer
-        csocket = context.csockets[self.server_name]
-        # get EPR socket to peer
-        epr_socket = context.epr_sockets[self.server_name]
+        csocket: Socket = context.csockets[client]
+        epr_socket: EPRSocket = context.epr_sockets[client]
+
+        # send a string message via a classical channel
+        # generate C and fix
+        csocket.send(C)
+        print(f"{ns.sim_time()} ns: Server sends message: {C}")
+
+        # Register a request to create an EPR pair, then apply a Hadamard gate on the epr qubit and measure
+        epr_qubit = epr_socket.create_keep()[0]
+        epr_qubit.H()
+        result = epr_qubit.measure()
+        yield from connection.flush()
+        print(f"{ns.sim_time()} ns: Server measures local EPR qubit: {result}")
+
+        
+        #merchant side
+        
+        msocket = context.msockets[merchant]
+
+       
+        
         # get connection to quantum network processing unit
         connection = context.connection
 
         # Bob listens for messages on his classical socket
-        message = yield from csocket.recv()
+        message = yield from msocket.recv()
         print(f"{ns.sim_time()} ns: Client: {self.name} receives message: {message}")
-
-        # Listen for request to create EPR pair, apply a Hadamard gate on the epr qubit and measure
-        epr_qubit = epr_socket.recv_keep()[0]
-        epr_qubit.H()
-        result = epr_qubit.measure()
-        yield from connection.flush()
-        print(
-            f"{ns.sim_time()} ns: Client: {self.name} measures local EPR qubit: {result}"
-        )
 
         return {}
