01892739406d11d87eabb7ff3653e5637904a334
Christwelve <meng.christian28@gmail.com>
2024-02-15 15:56:07 +0100

    added Marchant.py

---
+++ Merchant.py
@@ -0,0 +1,40 @@
+from typing import List
+
+import netsquid as ns
+from netqasm.sdk.classical_communication.socket import Socket
+from netqasm.sdk.connection import BaseNetQASMConnection
+from netqasm.sdk.epr_socket import EPRSocket
+
+from squidasm.sim.stack.program import Program, ProgramContext, ProgramMeta
+
+class MerchantProgram(Program):
+    def __init__(self, client: str, bank: str):
+        self.client = client
+        self.bank = bank
+
+    @property
+    def meta(self) -> ProgramMeta:
+        return ProgramMeta(
+            name="tutorial_program",
+            csockets=[self.client, self.bank]
+        )
+        
+
+    def run(self, context: ProgramContext):
+        # get classical socket to peer
+        bankCsocket = context.csockets[self.bank]
+        clientCsocket = context.csockets[self.client]
+        # get connection to quantum network processing unit
+        connection = context.connection
+
+        # Merchant listens for messages on his classical socket
+        clientMessage = clientCsocket.recv()
+        print(f"{ns.sim_time()} ns: Merchant receives from Client message: {clientMessage}")
+
+        messageToBank = clientMessage + ",MerchantID"
+        bankCsocket.send(messageToBank)
+        print(f"{ns.sim_time()} ns: Merchant sends to Bank message: {messageToBank}")
+    
+        connection.flush()
+
+        return {}
