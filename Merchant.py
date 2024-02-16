052e7da79233abfdd3f2b528cad3f6c6384babd8
Christwelve <meng.christian28@gmail.com>
2024-02-16 16:39:19 +0100

    restore

---
+++ Merchant.py
@@ -0,0 +1,43 @@
+from typing import List
+
+import netsquid as ns
+from netqasm.sdk.classical_communication.socket import Socket
+from netqasm.sdk.connection import BaseNetQASMConnection
+from netqasm.sdk.epr_socket import EPRSocket
+
+from squidasm.sim.stack.program import Program, ProgramContext, ProgramMeta
+
+class MerchantProgram(Program):
+    def __init__(self, parties: List[str], merchant_id: str):
+        self.parties = parties
+        self.merchant_id = merchant_id
+
+    @property
+    def meta(self) -> ProgramMeta:
+        return ProgramMeta(
+            name="sqdp",
+            csockets=self.parties,
+			epr_sockets=self.parties,
+            max_qubits=1,
+        )
+
+    def run(self, context: ProgramContext):
+        # get classical socket to peer
+        bankCsocket = context.csockets[self.parties[1]]
+        clientCsocket = context.csockets[self.parties[0]]
+        # get connection to quantum network processing unit
+        connection = context.connection
+
+        print("Merchant program started")
+
+        # Merchant listens for messages on his classical socket
+        clientMessage = yield from clientCsocket.recv()
+        print(f"{ns.sim_time()} ns: Merchant receives from Client message: {clientMessage}")
+
+        messageToBank = str(clientMessage) + " " + str(self.merchant_id)
+        bankCsocket.send(messageToBank)
+        print(f"{ns.sim_time()} ns: Merchant sends to Bank message: {messageToBank}")
+        
+        yield from connection.flush()
+
+        return {}
---
+++ run_SecureQuantumDigitalPayments.py
@@ -0,0 +1,52 @@
+from netsquid_magic.models.perfect import PerfectLinkConfig
+from netsquid_netbuilder.modules.clinks.default import DefaultCLinkConfig
+from netsquid_netbuilder.util.network_generation import create_complete_graph_network
+
+from squidasm.run.stack.run import run
+from Client import ClientProgram
+from Bank import BankProgram
+from Merchant import MerchantProgram
+
+import os
+import base64
+
+node_client_name = "Client"
+node_bank_name = "Bank"
+node_merchant_name = "Merchant1"
+nodeList = [node_client_name, node_bank_name,node_merchant_name]
+print(nodeList)
+
+# Merchant id should be 128 bit length
+#merchant_ids = [[merchant, base64.b64encode(os.urandom(16))] for merchant in node_merchants]
+merchant_id = base64.b64encode(os.urandom(16))
+lambda_parameter = 100
+C = os.urandom(32)
+
+M = [merchant_id]
+
+# import network configuration from file
+cfg = create_complete_graph_network(
+    nodeList,
+    "perfect",
+    PerfectLinkConfig(state_delay=100),
+    clink_typ="default",
+    clink_cfg=DefaultCLinkConfig(delay=100),
+)
+
+
+
+# programs = {
+#     node_client_name: ClientProgram(parties = [node_bank_name] + [node_merchant_name], lambda_parameter=lambda_parameter, C=C, M=M, client_name=node_client_name),
+#     node_bank_name: BankProgram(parties=[node_client_name] + [node_merchant_name], lambda_parameter=lambda_parameter, C=C),
+# 	node_merchant_name: MerchantProgram(parties = [node_client_name] + [node_bank_name], merchant_id = M[0])
+# }
+programs = {}
+programs[node_merchant_name] = MerchantProgram(parties = [node_client_name] + [node_bank_name], merchant_id = M[0])
+programs[node_bank_name] = BankProgram(parties=[node_client_name] + [node_merchant_name], lambda_parameter=lambda_parameter, C=C)
+programs[node_client_name] = ClientProgram(parties = [node_bank_name] + [node_merchant_name], lambda_parameter=lambda_parameter, C=C, M=M, client_name=node_client_name)
+
+#for merchant_id in merchant_ids:
+#    programs[merchant_id[0]] = MerchantProgram(parties = [node_client_name] + [node_bank_name], merchant_id = M[0])
+
+# Run the simulation. Programs argument is a mapping of network node labels to programs to run on that node
+run(config=cfg, programs=programs, num_times=1)
